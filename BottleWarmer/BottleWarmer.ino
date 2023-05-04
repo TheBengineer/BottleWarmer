86037ab5d49867a49c38361456e4b64ecbc1516a
Ben Holleran <wilddoogyplumb@gmail.com>
2023-05-04 18:07:19 -0400

    Encoder is causing instability, and doesn't work.

--- BottleWarmer/BottleWarmer.ino
+++ BottleWarmer/BottleWarmer.ino
@@ -6,6 +6,24 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "ESPRotary.h"
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+#include <ArduinoOTA.h>
+#include <sequencer4.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define SCREEN_SDA	4
+#define SCREEN_SCL	5
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
 #define ROTARY_PIN1	15
 #define ROTARY_PIN2	13
@@ -16,15 +34,53 @@
 
 #define TEMPERATURE_PIN	0
 
+#define DEVICE_NAME "BottleWarmer"
+
 
 
 OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature sensors(&oneWire);
 ESPRotary r;
 
+ESP8266WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdateServer;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup(void) {
   Serial.begin(74880);
+  Serial.println("BottleWarmer Starting");
+
+  WiFi.mode(WIFI_STA);
+  WiFiManager wifiManager;
+  if (!wifiManager.autoConnect(DEVICE_NAME)) {
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
+
+
+  ArduinoOTA.setPassword((const char*)"admin");
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
+
   sensors.begin();
 
   r.begin(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP);
@@ -33,6 +89,21 @@ void setup(void) {
   r.setRightRotationHandler(showDirection);
 
   pinMode(RELAY_PIN, OUTPUT);
+  pinMode(ROTARY_PIN1, INPUT);
+  pinMode(ROTARY_PIN2, INPUT);
+  pinMode(ROTARY_BUTTON, INPUT);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+  display.display();
+
 }
 
 void loop(void) {
@@ -42,7 +113,10 @@ void loop(void) {
   Serial.print(temperatureF);
   Serial.print("ºF, ");
   Serial.print(temperatureF2);
-  Serial.println("ºF");
+  Serial.print("ºF, ");
+  Serial.print(digitalRead(ROTARY_BUTTON));
+  Serial.print(", ");
+  Serial.println(r.getPosition());
   delay(500);
 }
 
