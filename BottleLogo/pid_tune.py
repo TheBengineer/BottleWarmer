a14ef3322dd9aaa4c084e964ea7c9d92fb7c8073
Ben Holleran <wilddoogyplumb@gmail.com>
2023-05-06 20:46:05 -0400

    Working on calculating simulation.

--- BottleLogo/pid_tune.py
+++ BottleLogo/pid_tune.py
@@ -10,11 +10,13 @@ starting_temperature = 65  # degrees F
 end_temperature = 209  # degrees F
 cooling_coefficient = 0.001
 e = 2.71828
+max_rise_rate = 0.2  # degrees F / second
 
 times = np.arange(0.0, max_time, time_step)
 temperatures = []
 
 actual_temperatures = []
+average_temperatures = []
 pwm_value = []
 actual_times = []
 starting_time = datetime.strptime("23:59:01", "%H:%M:%S")
@@ -25,6 +27,8 @@ with open('temperature_rise.json', 'r') as f:
         if on_time < 0:
             on_time += 24 * 60 * 60
         actual_temperatures.append(data['t1'])
+        last_temperature = average_temperatures[-1] if len(average_temperatures) > 0 else data['t1']
+        average_temperatures.append((last_temperature * .9) + (data['t1'] * .1))
         pwm_value.append(data['pwm'])
         actual_times.append(on_time)
 
@@ -46,7 +50,10 @@ for sample_time in times:
     current_temperature = temperature(sample_time)
     temperatures.append(current_temperature)
 
-plt.plot(times, temperatures)
-plt.plot(actual_times, actual_temperatures)
-plt.plot(actual_times, pwm_value)
+# plt.plot(times, temperatures)
+# plt.plot(actual_times, actual_temperatures)
+temperature_derivative = np.gradient(actual_temperatures, actual_times)
+plt.plot(actual_times, average_temperatures)
+#plt.plot(actual_times, temperature_derivative)
+# plt.plot(actual_times, pwm_value)
 plt.show()
