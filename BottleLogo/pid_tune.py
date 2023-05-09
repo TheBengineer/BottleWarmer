98972b2ac533b96010e6ad17d831fb49c08a2cef
Ben Holleran <wilddoogyplumb@gmail.com>
2023-05-09 08:59:31 -0400

    Plot by time and added goal

--- BottleLogo/pid_tune.py
+++ BottleLogo/pid_tune.py
@@ -20,20 +20,30 @@ average_temperatures = []
 env_temperatures = []
 pwm_value = []
 actual_times = []
-starting_time = datetime.strptime("23:59:01", "%H:%M:%S")
+target_temps = []
+
 with open('temperature_rise.json', 'r') as f:
-    for line in f:
+    lines = f.readlines()
+    first_line = json.loads(lines[0])
+    starting_time = datetime.strptime(first_line["time"], "%H:%M:%S")
+    last_time = starting_time
+    for line in lines:
         data = json.loads(line)
-        on_time = (datetime.strptime(data['time'], "%H:%M:%S") - starting_time).total_seconds()
+        on_time = datetime.strptime(data['time'], "%H:%M:%S")
+        if 60 > (last_time - on_time).total_seconds() > -60:
+            continue
+        lat_time = on_time
         if on_time:
-            if on_time < 0:
-                on_time += 24 * 60 * 60
             actual_temperatures.append(data['t1'])
             env_temperatures.append(data['t2'])
             last_temperature = average_temperatures[-1] if len(average_temperatures) > 0 else data['t1']
             average_temperatures.append((last_temperature * .9) + (data['t1'] * .1))
             pwm_value.append(data['pwm'])
             actual_times.append(on_time)
+            if on_time.hour == data["st_h"]:
+                target_temps.append(data["hot"])
+            else:
+                target_temps.append(data["set"])
 
 
 def temperature(run_time):
@@ -53,12 +63,14 @@ for sample_time in times:
     current_temperature = temperature(sample_time)
     temperatures.append(current_temperature)
 
-temperature_derivative = np.gradient(actual_temperatures, actual_times)
+# temperature_derivative = np.gradient(actual_temperatures, actual_times)
 
-#plt.plot(times, temperatures)
+# plt.plot(times, temperatures)
+plt.plot(actual_times, target_temps)
 plt.plot(actual_times, actual_temperatures)
-#plt.plot(actual_times, env_temperatures)
+# plt.plot(actual_times, env_temperatures)
 # plt.plot(actual_times, average_temperatures)
 # plt.plot(actual_times, temperature_derivative)
 # plt.plot(actual_times, pwm_value)
+plt.gcf().autofmt_xdate()
 plt.show()
